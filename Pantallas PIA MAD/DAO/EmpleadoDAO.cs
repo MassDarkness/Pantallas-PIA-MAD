b0d7fc4129b87c07f05a901c99d9fad1bcf59dea
JESUS ANTONIO GONZALEZ LEAL <antonio.gonzalezl@uanl.edu.mx>
2025-11-02 00:21:22 -0600

    prueba de puesto y departamento

---
+++ Pantallas PIA MAD/DAO/EmpleadoDAO.cs
@@ -0,0 +1,95 @@
+﻿using Pantallas_PIA_MAD.entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pantallas_PIA_MAD.DAO
+{
+    public class EmpleadoDAO
+    {
+        // Método para insertar un empleado usando el stored procedure
+        public static int InsertarEmpleado(Empleado empleado)
+        {
+            int retorno = 0;
+
+            using (SqlConnection conexion = BDConexion.ObtenerConexion())
+            {
+                using (SqlCommand comando = new SqlCommand("sp_RegistrarEmpleado", conexion))
+                {
+                    comando.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    // Parámetros
+                    comando.Parameters.AddWithValue("@numero_empleado", empleado.numero_empleado);
+                    comando.Parameters.AddWithValue("@nombres", empleado.nombres);
+                    comando.Parameters.AddWithValue("@apellido_paterno", empleado.apellido_paterno);
+                    comando.Parameters.AddWithValue("@apellido_materno", (object)empleado.apellido_materno ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@domicilio", (object)empleado.domicilio ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@telefono", (object)empleado.telefono ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@email", (object)empleado.email ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@fecha_nacimiento", (object)empleado.fecha_nacimiento ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@curp", empleado.curp);
+                    comando.Parameters.AddWithValue("@rfc", empleado.rfc);
+                    comando.Parameters.AddWithValue("@nss", empleado.nss);
+                    comando.Parameters.AddWithValue("@salario", (object)empleado.salario ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@salario_diario_integrado", (object)empleado.salario_diario_integrado ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@numero_cuenta", (object)empleado.numero_cuenta ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@banco", (object)empleado.banco ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@id_puesto", (object)empleado.id_puesto ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@id_departamento", (object)empleado.id_departamento ?? DBNull.Value);
+                    comando.Parameters.AddWithValue("@id_empresa", empleado.id_empresa);
+
+                    retorno = comando.ExecuteNonQuery();
+                }
+            }
+
+            return retorno;
+        }
+
+        // Método para obtener todos los empleados
+        public static List<Empleado> ObtenerEmpleados()
+        {
+            List<Empleado> lista = new List<Empleado>();
+
+            using (SqlConnection conexion = BDConexion.ObtenerConexion())
+            {
+                string query = "SELECT id_empleado, numero_empleado, nombres, apellido_paterno, apellido_materno, domicilio, telefono, email, fecha_nacimiento, curp, rfc, nss, salario, salario_diario_integrado, numero_cuenta, banco, id_puesto, id_departamento, id_empresa FROM Empleado;";
+                SqlCommand comando = new SqlCommand(query, conexion);
+
+                using (SqlDataReader reader = comando.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Empleado empleado = new Empleado();
+
+                        empleado.id_empleado = reader.GetInt32(0);
+                        empleado.numero_empleado = reader.IsDBNull(1) ? null : reader.GetString(1);
+                        empleado.nombres = reader.IsDBNull(2) ? null : reader.GetString(2);
+                        empleado.apellido_paterno = reader.IsDBNull(3) ? null : reader.GetString(3);
+                        empleado.apellido_materno = reader.IsDBNull(4) ? null : reader.GetString(4);
+                        empleado.domicilio = reader.IsDBNull(5) ? null : reader.GetString(5);
+                        empleado.telefono = reader.IsDBNull(6) ? null : reader.GetString(6);
+                        empleado.email = reader.IsDBNull(7) ? null : reader.GetString(7);
+                        empleado.fecha_nacimiento = reader.IsDBNull(8) ? (DateTime?)null : reader.GetDateTime(8);
+                        empleado.curp = reader.IsDBNull(9) ? null : reader.GetString(9);
+                        empleado.rfc = reader.IsDBNull(10) ? null : reader.GetString(10);
+                        empleado.nss = reader.IsDBNull(11) ? null : reader.GetString(11);
+                        empleado.salario = reader.IsDBNull(12) ? (decimal?)null : reader.GetDecimal(12);
+                        empleado.salario_diario_integrado = reader.IsDBNull(13) ? (decimal?)null : reader.GetDecimal(13);
+                        empleado.numero_cuenta = reader.IsDBNull(14) ? null : reader.GetString(14);
+                        empleado.banco = reader.IsDBNull(15) ? null : reader.GetString(15);
+                        empleado.id_puesto = reader.IsDBNull(16) ? (int?)null : reader.GetInt32(16);
+                        empleado.id_departamento = reader.IsDBNull(17) ? (int?)null : reader.GetInt32(17);
+                        empleado.id_empresa = reader.GetInt32(18);
+
+                        lista.Add(empleado);
+                    }
+                }
+            }
+
+            return lista;
+        }
+    }
+}
--- Pantallas PIA MAD/Form6.cs
+++ Pantallas PIA MAD/Form6.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Pantallas_PIA_MAD.DAO;
+using Pantallas_PIA_MAD.entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,105 @@ namespace Pantallas_PIA_MAD
         public Form6()
         {
             InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            RefrescarEmpleados();
+            CargarEmpresas();
+        }
+        private void CargarEmpresas()
+        {
+            // Obtener empresas desde la base de datos
+            var empresas = EmpresaDAP.ObtenerEmpresas();
+
+            ComboBoxEmpresa.DataSource = empresas;
+            ComboBoxEmpresa.DisplayMember = "nombre"; // lo que se verá en el combo
+            ComboBoxEmpresa.ValueMember = "id_empresa"; // el ID real
+            ComboBoxEmpresa.SelectedIndex = -1; // opción inicial vacía
+        }
+
+        private void ComboBoxEmpresa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ComboBoxEmpresa.SelectedIndex != -1)
+            {
+                int idEmpresa = (int)ComboBoxEmpresa.SelectedValue;
+                CargarDepartamentos(idEmpresa);
+            }
+            else
+            {
+                ComboBoxDepartamento.DataSource = null;
+                ComboBoxPuesto.DataSource = null;
+            }
+        }
+
+        private void CargarDepartamentos(int idEmpresa)
+        {
+            // Obtener departamentos por empresa
+            var departamentos = DepartamentoDAO.ObtenerDepartamentosPorEmpresa(idEmpresa);
+
+            ComboBoxDepartamento.DataSource = departamentos;
+            ComboBoxDepartamento.DisplayMember = "nombre"; // nombre del departamento
+            ComboBoxDepartamento.ValueMember = "id_departamento";
+            ComboBoxDepartamento.SelectedIndex = -1;
+        }
+
+        private void ComboBoxDepartamento_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ComboBoxDepartamento.SelectedIndex != -1)
+            {
+                int idDepartamento = (int)ComboBoxDepartamento.SelectedValue;
+                CargarPuestos(idDepartamento);
+            }
+            else
+            {
+                ComboBoxPuesto.DataSource = null;
+            }
+        }
+
+        private void CargarPuestos(int idDepartamento)
+        {
+            var puestos = PuestoDAO.ObtenerPuestosPorDepartamento(idDepartamento);
+
+            ComboBoxPuesto.DataSource = puestos;
+            ComboBoxPuesto.DisplayMember = "nombre"; // nombre del puesto
+            ComboBoxPuesto.ValueMember = "id_puesto";
+            ComboBoxPuesto.SelectedIndex = -1;
+        }
+
+
+        private void BTN_AgregarEMP_Click(object sender, EventArgs e)
+        {
+            Empleado empleado = new Empleado
+            {
+                numero_empleado = TB_NumEmp.Text,
+                nombres = TB_Nombres.Text,
+                apellido_paterno = TB_APPaterno.Text,
+                apellido_materno = TB_APMaterno.Text,
+                domicilio = TB_Domicilio.Text,
+                telefono = TB_Telefono.Text,
+                email = TB_Email.Text,
+                fecha_nacimiento = FechaNac_EMP.Value.Date,
+                curp = TB_CURP.Text,
+                rfc = TB_RFC.Text,
+                nss = TB_NSS.Text,
+                salario = string.IsNullOrEmpty(TB_SalarioDiario.Text) ? (decimal?)null : decimal.Parse(TB_SalarioDiario.Text),
+                salario_diario_integrado = string.IsNullOrEmpty(TB_SDInte.Text) ? (decimal?)null : decimal.Parse(TB_SDInte.Text),
+                numero_cuenta = TB_NumCuenta.Text,
+                banco = TB_Banco.Text,
+                id_empresa = (int)ComboBoxEmpresa.SelectedValue,
+                id_departamento = (int)ComboBoxDepartamento.SelectedValue,
+                id_puesto = (int)ComboBoxPuesto.SelectedValue
+            };
+
+            RefrescarEmpleados();
+        }
+
+
+        // Método para actualizar el DataGridView
+        private void RefrescarEmpleados()
+        {
+            Vista_EMP.DataSource = EmpleadoDAO.ObtenerEmpleados();
         }
     }
 }
