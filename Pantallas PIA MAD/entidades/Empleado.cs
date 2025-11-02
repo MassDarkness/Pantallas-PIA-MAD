4d1bb0be714cab4dc06ce79a033d182af52032e3
JESUS ANTONIO GONZALEZ LEAL <antonio.gonzalezl@uanl.edu.mx>
2025-11-01 23:08:06 -0600

    listo

---
+++ Pantallas PIA MAD/entidades/Empleado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pantallas_PIA_MAD.entidades
+{
+    public class Empleado
+    {
+        public int id_empleado { get; set; }
+        public string numero_empleado { get; set; }
+        public string nombres { get; set; }
+
+    }
+}
