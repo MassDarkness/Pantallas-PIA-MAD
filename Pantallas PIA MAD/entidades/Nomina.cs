e7eabdbc3bd458a18a3dd3f1a40bb111dfe062ab
ANGEL ANTONIO GONZALEZ TORRES <antonio.gonzalezt@uanl.edu.mx>
2025-11-01 23:27:02 -0600

    NOMINA HECHOOOOO

---
+++ Pantallas PIA MAD/entidades/Nomina.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pantallas_PIA_MAD.entidades
+{
+    public class Nomina
+    {
+        public int id_nomina { get; set; }
+        public DateTime? fecha { get; set; }
+        public string estatus { get; set; }
+        
+        //Llave Foranea
+        public int? id_empleado { get; set; }
+
+        public Nomina() { }
+
+        public Nomina(int id_nomina, DateTime? fecha, string estatus, int? id_empleado)
+        {
+            this.id_nomina = id_nomina;
+            this.fecha = fecha;
+            this.estatus = estatus;
+            this.id_empleado = id_empleado;
+        }
+    }
+}
