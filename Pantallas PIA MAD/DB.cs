3962d875274171520dba9c00dd75f7fc5e646034
JESUS ANTONIO GONZALEZ LEAL <antonio.gonzalezl@uanl.edu.mx>
2025-11-01 21:15:07 -0600

    alo alo prueba

---
+++ Pantallas PIA MAD/DB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pantallas_PIA_MAD
+{
+    public class BDConexion
+    {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ProyectoMAD;Data Source=CHUYGONZA_LAP");
+
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
---
+++ Pantallas PIA MAD/RegistroEmpresa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pantallas_PIA_MAD
+{
+    public class RegistroEmpresa
+    {
+    public int id_empresa { get; set; }
+    public string razon_social { get; set; }
+    public string domicilio_fiscal { get; set; }
+    public string contacto { get; set; }
+    public string registro_patronal { get; set; }
+    public string RFC { get; set; }
+     
+    public DateTime fecha_inicio_operaciones { get; set; }
+
+    }
+}
