8a8e960d7355ca662256ff6a7aa57f2db38508a3
ANGEL ANTONIO GONZALEZ TORRES <antonio.gonzalezt@uanl.edu.mx>
2025-11-01 23:13:13 -0600

    Departamento terminado

---
+++ Pantallas PIA MAD/entidades/Departamento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pantallas_PIA_MAD.entidades
+{
+    public class Departamento
+    {
+        public int id_departamento { get; set; }
+        string nombre { get; set; }
+
+        int numero { get; set; }
+
+        public Departamento() { }
+
+
+        public Departamento(int id_departamento, string nombre, int numero)
+        {
+            this.id_departamento = id_departamento;
+            this.nombre = nombre;
+            this.numero = numero;
+        }
+    }
+}
