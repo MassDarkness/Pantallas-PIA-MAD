c7f86fac04b0529ca6659dcb87dc5006a1464f00
ANGEL ANTONIO GONZALEZ TORRES <antonio.gonzalezt@uanl.edu.mx>
2025-11-01 22:51:02 -0600

    FORM2 HECHO

--- Pantallas PIA MAD/Form2.cs
+++ Pantallas PIA MAD/Form2.cs
@@ -17,9 +17,53 @@ namespace Pantallas_PIA_MAD
             InitializeComponent();
         }
 
+        private void Form2_Load(object sender, EventArgs e)
+        {
+        }
+
         private void BTN_IS_Click(object sender, EventArgs e)
         {
+            string correo = IS_Correo.Text;
+            string contrasena = IS_Contraseña.Text;
 
+            if (RB_ADMIN.Checked)
+            {
+                if (correo == "admin@correo.com" && contrasena == "admin123")
+                {
+                    MessageBox.Show("¡Bienvenido ADMIN!");
+                    Form3 menuAdmin = new Form3();
+                    menuAdmin.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Correo o contraseña incorrectos.", "Error de inicio de sesión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (RB_AUXILIAR.Checked)
+            {
+                if (correo == "auxiliar@correo.com" && contrasena == "auxiliar123")
+                {
+                    MessageBox.Show("¡Bienvenido AUXILIAR!");
+                    Form13 menuAuxiliar = new Form13();
+                    menuAuxiliar.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Correo o contraseña incorrectos.", "Error de inicio de sesión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona un tipo de usuario (ADMIN o AUXILIAR).", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void BTN_SALIRIS_Click(object sender, EventArgs e)
+        {
+            // Cierra toda la aplicación
+            Application.Exit();
         }
     }
 }
